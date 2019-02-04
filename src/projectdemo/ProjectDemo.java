4ce4bd9a6d5ccd38c32ae58429f3eaa2f2fcc304
Nick <nsoltysiak@gmail.com>
2019-02-04 16:12:33 -0500

    Initial skeleton of the project

---
+++ src/projectdemo/ProjectDemo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectdemo;
+
+/**
+ *
+ * @author Nick
+ */
+public class ProjectDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
